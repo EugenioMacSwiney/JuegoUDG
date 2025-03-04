9be70277e7a3142d97875fdd8b73f2d0ff348883
EugenioMacSwiney <eugenio.mac@udgvirtual.udg.mx>
2025-03-03 21:43:38 -0600

    3.2. Los prefabs

--- JuegoUDG/Assets/Scripts/JhonMovement.cs
+++ JuegoUDG/Assets/Scripts/JhonMovement.cs
@@ -1,35 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class JhonMovement : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float Horizontal;
+    private bool Grounded;
+
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
-    void Update() 
+    void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else if (Horizontal > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+
+        // Detección de suelo con Raycast
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+        Grounded = hit.collider != null; // Si el rayo golpea algo, está en el suelo
+
+        // Comprobar si se presiona la tecla de salto y está en el suelo
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
         {
             Jump();
         }
+
+        // Depuración para verificar si está detectando el suelo
+        Debug.Log("Grounded: " + Grounded);
     }
+
     private void Jump()
     {
-        Rigidbody2D.AddForce(Vector2.up, ForceMode2D.Impulse);
+        // Aplicar fuerza hacia arriba multiplicada por JumpForce
+        Rigidbody2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D.linearVelocity = new Vector2(Horizontal * JumpForce, Rigidbody2D.linearVelocity.y);
+        // Movimiento horizontal
         Rigidbody2D.linearVelocity = new Vector2(Horizontal * Speed, Rigidbody2D.linearVelocity.y);
 }
 }
+
