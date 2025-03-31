f50cfd60a969643a84a1651276cd0eb7a6f3c93e
EugenioMacSwiney <eugenio.mac@udgvirtual.udg.mx>
2025-03-31 08:46:33 -0600

    3.9. Las pruebas de resultados: el paso final

---
+++ JuegoUDG/Assets/Scripts/Exit.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Game is exiting");
+        }
+    }
+}
